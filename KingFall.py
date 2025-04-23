6ee81ee5e6a86f882a38142f9e95b61ff8f09161
Brandon Dean <brandondean1068@gmail.com>
2025-04-23 14:00:23 -0400

    Final Commit

--- KingFall.py
+++ KingFall.py
@@ -22,22 +22,27 @@ def isTerminal(people, tech, karma, year):
         print("The people hate you now, sorry you lose")
         print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
         exit(0)
+
     elif people >= 5:
         print("The people are in love with you and you can do no wrong. You win!!")
         print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
         exit(0)
+
     if karma <= -5:
         print("You hate yourself for the things you've done, I'm sorry but you lose")
         print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
         exit(0)
+
     elif karma >= 5:
         print("You feel great about yourself and feel like retiring. You win!!")
         print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
         exit(0)
+
     if tech <= -5:
         print("The technological world has not progressed and you will lose your job. I'm sorry bout that loss")
         print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
         exit(0)
+        
     elif tech >= 5:
         print("Technology is flourishing and you're the godfather of it. You win!!")
         print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
@@ -51,30 +56,42 @@ def play():
     initial_in = input("Challenge mode or coninuous?: ")
 
     if initial_in.lower() == 'challenge':
+
         while (usr.people, usr.tech, usr.karma > -5):
+
             people, tech, karma = usr.getInfo()
             print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+
             dialogue, p, t, k = ops.find_dialogue_option()
             print(dialogue)
+
             userin = input("Yes or No: ")
             if userin.lower()=='yes':
                 usr.change(p, t, k)
             if userin.lower()=='no':
                 usr.change(-p, -t, -k)
+
             year+=1
             isTerminal(usr.people, usr.tech, usr.karma, year)
+
     elif initial_in.lower() == 'continuous':
+
         while True:
+            
             people, tech, karma = usr.getInfo()
             print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+
             dialogue, p, t, k = ops.find_dialogue_option()
             print(dialogue)
+
             userin = input("Yes, No or q to quit: ")
             if userin.lower()=='yes':
                 usr.change(p, t, k)
             if userin.lower()=='no':
                 usr.change(-p, -t, -k)
+
             year+=1
+
             if userin.lower()=='q':
                 print(f"FINAL {year}:\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
                 print("I hope you had fun, and have a good day")
