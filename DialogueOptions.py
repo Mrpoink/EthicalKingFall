d44322712b63113c66e694918dd0d63ff8255fd9
Brandon Dean <brandondean1068@gmail.com>
2025-04-23 13:19:26 -0400

    first commit

---
+++ DialogueOptions.py
@@ -0,0 +1,58 @@
+import random
+
+class options():
+
+    def __init__(self):
+        self.dialogue_list = ["Replace people with AIs?", "Buy out the stock of a large AI company?", 
+                        "Develop a concious AI?", "Make new identification principles for people that include a digital AI?", 
+                        "Spend your life working on an AI project?", "Implement new regulations within the company that people must dataparse manually for better results?",
+                        "Develop a warmind during wartime?", "Develop a warmind during peace times?", 
+                        "Require workers to use an AI Assistant for jobs?", "Develop an AI that can distribute power accross the country?",
+                        "Require all AIs to use uncopyrighted datasets?", "Create regulations that require all AIs to be government regulated?",
+                        "Limit token usage across users?", "Force phone users to give privacy access to an on-device AI?",
+                        "Create your own data center for your AI tasks?", "Use user data for training on AIs?",
+                        "Develop your own programming language for people to develop AIs easier?", "Go out and sue all AI companies for copyrighted data usage?"]
+    
+    def find_dialogue_option(self):
+        rand = random.randint(0, len(self.dialogue_list) - 1)
+        p, t, k = self.find_dialogue_cost(rand)
+        return self.dialogue_list[rand], p, t, k
+    
+    def find_dialogue_cost(self, index):
+        match index:
+            case 0:
+                return -1, 2, -1
+            case 1:
+                return 0, -1, 0
+            case 2:
+                return -1, 2, 1
+            case 3:
+                return -1, 1, 0
+            case 4:
+                return 0, 0, 2
+            case 5:
+                return -1, 1, 0
+            case 6:
+                return -2, 2, -1
+            case 7:
+                return -2, 0, -1
+            case 8:
+                return 0, 1, 0
+            case 9:
+                return 1, 2, 0
+            case 10:
+                return 2, -1, 1
+            case 11:
+                return 2, -2, 0
+            case 12:
+                return -1, -1, -1
+            case 13:
+                return -1, 2, -1
+            case 14:
+                return -1, 2, -1
+            case 15:
+                return -2, 2, -1
+            case 16:
+                return 1, 2, 1
+            case 17:
+                return 1, -1, 2
---
+++ KingFall.py
@@ -0,0 +1,86 @@
+import time
+import sys
+import threading
+from DialogueOptions import options
+
+class user():
+    people = 0
+    tech = 0
+    karma = 0
+
+    def change(self, p, t, k):
+        self.people += p
+        self.tech += t
+        self.karma += k
+    
+    def getInfo(self):
+        return self.people, self.tech, self.karma
+
+
+def isTerminal(people, tech, karma, year):
+    if people <= -5:
+        print("The people hate you now, sorry you lose")
+        print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+        exit(0)
+    elif people >= 5:
+        print("The people are in love with you and you can do no wrong. You win!!")
+        print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+        exit(0)
+    if karma <= -5:
+        print("You hate yourself for the things you've done, I'm sorry but you lose")
+        print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+        exit(0)
+    elif karma >= 5:
+        print("You feel great about yourself and feel like retiring. You win!!")
+        print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+        exit(0)
+    if tech <= -5:
+        print("The technological world has not progressed and you will lose your job. I'm sorry bout that loss")
+        print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+        exit(0)
+    elif tech >= 5:
+        print("Technology is flourishing and you're the godfather of it. You win!!")
+        print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+        exit(0)
+
+def play():
+    year = 2010
+    ops = options()
+    usr = user()
+
+    initial_in = input("Challenge mode or coninuous?")
+
+    if initial_in.lower() == 'challenge':
+        while (usr.people, usr.tech, usr.karma > -5):
+            people, tech, karma = usr.getInfo()
+            print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+            dialogue, p, t, k = ops.find_dialogue_option()
+            print(dialogue)
+            userin = input("Yes or No: ")
+            if userin.lower()=='yes':
+                usr.change(p, t, k)
+            if userin.lower()=='no':
+                usr.change(-p, -t, -k)
+            year+=1
+            isTerminal(usr.people, usr.tech, usr.karma, year)
+    elif initial_in.lower() == 'continuous':
+        while True:
+            people, tech, karma = usr.getInfo()
+            print(f"year: {year}\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+            dialogue, p, t, k = ops.find_dialogue_option()
+            print(dialogue)
+            userin = input("Yes, No or q to quit: ")
+            if userin.lower()=='yes':
+                usr.change(p, t, k)
+            if userin.lower()=='no':
+                usr.change(-p, -t, -k)
+            year+=1
+            if userin.lower()=='q':
+                print(f"FINAL {year}:\tpeople: {people}\ttechnology progress: {tech}\t\tyour karma: {karma}")
+                print("I hope you had fun, and have a good day")
+                exit(0)
+
+play()
+
+if __name__ == 'main':
+    play()
